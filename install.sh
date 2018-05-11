ca8ff3eb1732edbaf45be71306b94ecd444922ee
rajethicalhacker <31346934+rajethicalhacker@users.noreply.github.com>
2018-05-11 13:35:37 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 git clone https://github.com/rajethicalhacker/auto-msf.git
 chmod +x auto-msf/script.py
+chmod +x auto-msf/auto-msf-ngrok
 mv auto-msf/script.py /usr/bin/auto-msf
+mv auto-msf/auto-msf-ngrok /usr/bin/auto-msf-ngrok 
 echo "execute auto-msf on your terminal"
+echo "Use auto-msf-ngrok for ngrok version"
